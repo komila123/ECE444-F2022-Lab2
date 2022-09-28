cae125cc9f938f8543dd166b3fdfbb5fcba4b6a6
komila123 <komila_rasulova@yahoo.com>
2022-09-28 00:06:22 -0400

    Activity 4 with email and name requirements

--- README.md
+++ README.md
@@ -14,4 +14,14 @@ Activity 2
 Activity 3
 ![](Images/Activity3.png)
 
+Activity 4
+![](Images/Activity4.png)
+
+![](Images/Activity4.1.png)
+
+![](Images/Activity4.2.png)
+
+![](Images/Activity4.3.png)
+
+
 
--- hello.py
+++ hello.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired, ValidationError, Email
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -14,6 +14,7 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[Email()])
     submit = SubmitField('Submit')
 
 @app.errorhandler(404)
@@ -23,13 +24,18 @@ def page_not_found(e):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+    name = None
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html',form = form, name = session.get('name'), current_time=datetime.utcnow())
+    return render_template('index.html',form = form, name = session.get('name'), current_time=datetime.utcnow(),  email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
